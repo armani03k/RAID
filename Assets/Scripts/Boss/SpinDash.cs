270cdb7d8d041f89773a802d5c30c2cf6be47199
AvenceCryaxlyx <kennybyron.lin@benilde.edu.ph>
2018-04-23 23:03:04 +0800

    Boss Battle First Attack Pattern in progress
    Pattern 1
    
    Initial Behavior 80% done - missing taunt after set number of bounces and subject for QA.
    
    Disruption Reaction 0%
    Low health behavior 0%

---
+++ Assets/Scripts/Boss/SpinDash.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinDash : AttackPattern {
+
+    public float ChargeUpDelay;
+    public float DashDelay;
+    public float Speed;
+    public int NumberOfBounces;
+
+    bool m_dash;
+    
+    BossAI m_bossAI;
+    Vector2 m_direction;
+    int m_numberOfBounces;
+	// Use this for initialization
+	void Start () {
+        m_bossAI = GetComponent<BossAI>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_numberOfBounces > NumberOfBounces)
+            m_isFinished = true;
+
+        if (m_dash)
+        {
+            m_bossAI.GetRigidBody.velocity = (m_direction * Speed * Time.deltaTime);
+        }
+            
+	}
+
+    public override IEnumerator Attack()
+    {
+        
+        new WaitForSeconds(ChargeUpDelay);
+        m_bossAI.GetAnimator.SetBool("Attack", true);
+        m_bossAI.GetAnimator.SetFloat("AttackIndex", 0);
+        yield return null;
+    }
+
+    void DashTowards()
+    {
+        m_direction = target.transform.position - transform.position;
+        m_numberOfBounces++;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") && !m_isFinished)
+            DashTowards();
+    }
+
+    public void BallTransform()
+    {
+        m_bossAI.GetAnimator.SetFloat("AttackIndex", 1);
+        DashTowards();
+        m_dash = true;
+    }
+
+    public override void EndAttack()
+    {
+        m_bossAI.GetAnimator.SetBool("Attack", false);
+        m_bossAI.GetRigidBody.velocity = Vector2.zero;
+        base.EndAttack();
+        Debug.Log("End");
+        m_dash = false;
+        m_numberOfBounces = 0;
+        m_direction = Vector2.zero;
+
+    }
+}
---
+++ Assets/TestControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += movement/2;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+	}
+}
